8aa0e718625b0009f410837999c716061b5e6af0
takaki <cheap_trick_magic@yahoo.co.jp>
2021-01-14 13:43:57 +0900

    Administration Middlewareの作成

---
+++ laravel/app/Http/Middleware/AdminAuthentication.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Contracts\Auth\Guard; // Authを利用するためのuse
+use Illuminate\Http\RedirectResponse; // リダイレクトクラスを利用するためのuse
+use Closure;
+
+class AdminAuthentication
+{
+    protected $auth;
+
+    public function __construct(Guard $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($this->auth->check()) // リクエストするユーザーがログインしているかどうか
+        {
+            if ($this->auth->user()->is_admin == true) // ユーザーが管理者かどうか
+            {
+                return $next($request);
+            }
+        }
+        // 管理者でない場合はリダイレクトでTOPページへ戻る
+        return new RedirectResponse(url('/'));
+    }
+}
